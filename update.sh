ad02758cf79aa9a8d4f0c16bc39f4a42ff821e8e
jksherk <71897861+jksherk@users.noreply.github.com>
2024-06-28 10:56:51 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-# 闲鱼ID：胡子哥爱游戏
+# 闲鱼ID搜索：胡子哥爱游戏 再点击用户 即可找到产品
 chmod +x /home/ps2/update
-/home/ps2/update
 
-# 激活PS1游戏转换
-rm -f /home/ps2/UPDATE110
-rm -f /home/ps2/UPDATEP
-rm -f /home/ps2/UPDATECH
+cat << EOF | sudo tee /etc/systemd/system/updatehuzige.service
+[Unit]
+Description=Update huzige Service
+After=network.target
 
-# 支持chd 多个bin文件转换
-rm -f /home/ps2/update
+[Service]
+ExecStart=/home/ps2/update
+Type=simple
+Restart=on-failure
+RestartSec=5s
 
-# 自动适配新的PS1模拟器
-rm -f -- "$0"
+[Install]
+WantedBy=multi-user.target
+EOF
+
+## 激活PS1游戏转换 支持chd 多个bin文件转换 自动适配新的PS1模拟器 有问题欢迎咨询 B站号：胡子哥爱游戏
+sudo systemctl daemon-reload
+
+sudo systemctl enable updatehuzige.service
+
+/bin/rm -- "$0"
+
+sudo reboot
